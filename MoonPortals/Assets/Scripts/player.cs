b52bab497a61fafbfa16d149f45e24b9f3254164
Hasini <hasinisama99@gmail.com>
2021-03-19 15:19:54 +0530

    Player Controls Added

---
+++ MoonPortals/Assets/Scripts/player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //[SerializeField]private Transform groundCheckTransform = null;
+    public bool isGrounded = true;  
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>(); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))   
+            jumpKeyWasPressed = true;
+
+        horizontalInput = Input.GetAxis("Horizontal");    
+    }
+
+    private void FixedUpdate(){
+        if(!isGrounded)
+        {
+            return;
+        }
+        /* 
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).length == 1)
+        {
+            return;
+        }*/
+
+        if (jumpKeyWasPressed)
+        {
+           
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+
+        }
+
+       rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+    }
+    private void OnCollisionEnter(Collision collision) {
+        isGrounded = true;
+    }
+    private void OnCollisionExit(Collision collision) {
+        isGrounded = false;    
+    }
+}
